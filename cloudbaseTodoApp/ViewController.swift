87e312b25c9a25955c227cf92f46fc8d3bcffa3a
radiney <99511595+radiney@users.noreply.github.com>
2022-11-09 18:42:01 -0800

    Update ViewController.swift

--- cloudbaseTodoApp/ViewController.swift
+++ cloudbaseTodoApp/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
                                           self?.saveToDo(item: text)
                 }
             }
-        ))
+        ))	
     }
     
     func removeItemFromDatabase(itemKey: String){
